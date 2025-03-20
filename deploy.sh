0500cc51897658604a4f70ef962028dbd6e577dd
Suganth <144556544+Suganthchandran@users.noreply.github.com>
2025-03-20 11:41:43 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,6 @@
     echo hi123
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u ar8888 -p dckr_pat_HU8f1bsQJp3j2DnCttXuuoppo7w
-    docker tag test ar8888/dev
-    docker push ar8888/dev
-    docker-compose up -d
-    
+    docker login -u suganth07 -p dckr_pat_oAYT02qRwSz2jrlHz54liPeI7_k
+    docker tag test suganth07/devops
+    docker push suganth07/devops
