6018969c49c3b9be240dd2f31250f40306e7902a
Suganth <144556544+Suganthchandran@users.noreply.github.com>
2025-03-20 11:42:26 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@
     sh 'chmod +x build.sh'
     sh './build.sh'
     docker login -u suganth07 -p dckr_pat_oAYT02qRwSz2jrlHz54liPeI7_k
-    docker tag test suganth07/devops
+    docker tag test1 suganth07/devops
     docker push suganth07/devops
